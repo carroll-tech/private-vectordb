9d6db3000f362d0434e5b8bea647d3f24b4331f5
jolfr <thomas.jack.carroll@gmail.com>
2024-11-07 10:05:13 -0500

    feat(container): can create table in lancedb on cluster

--- containers/seed-data/src/main.py
+++ containers/seed-data/src/main.py
@@ -1,7 +1,15 @@
+import asyncio
 import boto3
 import os
 import lance
+import lancedb
 from dotenv import load_dotenv
+
+def safe_load_env_var(var_name):
+    value = os.getenv(var_name)
+    if value is None or value == "":
+        raise EnvironmentError(f"The environment variable '{var_name}' is not set.")
+    return value
 
 def download_s3_folder(bucket_name, folder_name, local_dir):
     s3 = boto3.client('s3')
@@ -45,24 +53,63 @@ def load_dataframe_from_lance(path: str):
     except Exception as e:
         print(f"An error occurred: {e}")
         exit(1)
+        
+async def get_lancedb_client(bucket_name: str, access_key: str, secret_access_key: str, endpoint: str):
+    try:
+        db = await lancedb.connect_async(
+            f"s3://{bucket_name}",
+            storage_options={
+                "region": "us-east-1",
+                "aws_access_key_id": access_key,
+                "aws_secret_access_key": secret_access_key,
+                "endpoint": endpoint,
+                "allow_http": "true"
+            }
+        )
+        print("LanceDB client initialized")
+        return db
+    except Exception as e:
+        print(f"An error occurred while initializing LanceDB client: {e}")
+        exit(1)
+    
+async def create_table(client, table_name, dataframe):
+    try:
+        print(f"Creating table {table_name}")
+        table = await client.create_table(table_name, dataframe)
+        print(f"{table_name} created successfully")
+        return table
+    except Exception as e:
+        print(f"An error occurred while creating {table_name}: {e}")
+        exit(1)
 
 def main():
     load_dotenv()
     
     # Download example data
-    bucket_name = os.getenv("SEED_BUCKET_NAME", "pvdb-lance-seed")
-    folder_name = os.getenv("SEED_FOLDER_NAME", "vec_data.lance/")
-    local_dir = "./data.lance"
+    seed_bucket_name = os.getenv("SEED_BUCKET_NAME", "pvdb-lance-seed")
+    seed_folder_name = os.getenv("SEED_FOLDER_NAME", "vec_data.lance/")
+    seed_local_dir = "./data.lance"
     
-    path = download_s3_folder(bucket_name, folder_name, local_dir)
+    path = download_s3_folder(seed_bucket_name, seed_folder_name, seed_local_dir)
     
     # Load lance data as pandas dataframe
     df = load_dataframe_from_lance(path)
     
-    print(df.head())
+    # Get LanceDB client
+    bucket_name = safe_load_env_var("BUCKET_NAME")
+    bucket_host = safe_load_env_var("BUCKET_HOST")
+    bucket_port = safe_load_env_var("BUCKET_PORT")
+    bucket_access_key = safe_load_env_var("CEPH_ACCESS_KEY_ID")
+    bucket_secret_access_key = safe_load_env_var("CEPH_SECRET_ACCESS_KEY")
+    bucket_endpoint = f"http://{bucket_host}:{bucket_port}"
     
+    db = asyncio.run(get_lancedb_client(bucket_name, bucket_access_key, bucket_secret_access_key, bucket_endpoint))
     
-
+    # Create table
+    table = asyncio.run(create_table(db, "test_table_1", df))
+    
+    # Query table
+    print(asyncio.run(table.query().limit(5).to_arrow()))
 
 if __name__ == "__main__":
     main()
